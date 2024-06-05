74c452e4455d7f8f0a66f8106aa4f58f83bc5a31
eg40262 <eg40262@gmail.com>
2024-06-06 00:54:57 +0900

    編集するをクリックしたらindexが表示されるを解決

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,22 +1,32 @@
 class WorkoutsController < ApplicationController
   before_action :redirect_unless_logged_in, except: :index
+
   # before_action :authenticate_user! # ユーザーがログインしていることを確認
   #このフィルターにより、ユーザーがログインしていない場合、アクションが実行される前にログインページにリダイレクトされます。
   
   def index
     if user_signed_in?
-      @workouts = current_user.workouts.recent
+      if params[:year].present? && params[:month].present?
+        selected_year = params[:year].to_i
+        selected_month = params[:month].to_i
+        Rails.logger.debug "Selected year: #{selected_year}, Selected month: #{selected_month}"
+        @workouts = current_user.workouts.where("extract(year from date) = ? AND extract(month from date) = ?", selected_year, selected_month).order(date: :desc).recent
+        # @workouts = current_user.workouts.where("extract(year from date) = ? AND extract(month from date) = ?", selected_year, selected_month).recent
+      else
+        @workouts = current_user.workouts.order(date: :desc).recent
+        # @workouts = [] 多分これが表示されない原因だったのだと思う20240606
+      end
     else
-      # ログインしていない場合は何もしないか、別のメッセージを表示する
       @workouts = []
       flash[:alert] = "Please log in to view your workouts."
     end
+  end
     #@workouts = current_user.workouts.recent if user_signed_in?
     # 現在ログインしているユーザーに関連する workout オブジェクトをすべて取得します。
     #.recent はスコープメソッドでワークアウトのレコードを新しい順にソートして返す
     #@workouts = Workout.recent
     #@workouts = Workout.all
-  end
+
 
   def show
     @workout = Workout.find(params[:id])
@@ -32,14 +42,15 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    @workout = Workout.new(workout_params)
+    @workout = Workout.new(workout_params.except(:year, :month)) # yearとmonthを除外
+    # @workout = Workout.new(workout_params)
     @workout.user = current_user
     if @workout.save
       redirect_to @workout, notice: 'Workout was successfully created.'
       #redirect_to root_path
       #redirect_to @workout, notice: 'Workout was successfully created.'
     else
-      @exercise_count = workout_params[:exercise_count].to_i
+      # @exercise_count = workout_params[:exercise_count].to_i 20240605に消した
       render :new
     end
   end
@@ -71,7 +82,8 @@ class WorkoutsController < ApplicationController
   end
 
   def workout_params
-    params.require(:workout).permit(:date, :image, exercises_attributes: [:id, :name, :weight, :reps, :sets, :duration, :distance, :_destroy])
+    params.require(:workout).permit(:date, :image, exercises_attributes: [:id, :name, :weight, :reps, :sets, :duration, :_destroy])
+    # params.require(:workout).permit(:date, :image, exercises_attributes: [:id, :name, :weight, :reps, :sets, :duration, :distance, :_destroy])
     #params.require(:workout).permit(:date, exercises_attributes: [:id, :name, :weight, :reps, :sets, :duration, :distance, :_destroy])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'workouts#index'
 
   resources :workouts do
-    resources :exercises
+    resources :exercises, only: [:create, :update, :destroy]
+    collection do
+      get ':year/:month', to: 'workouts#index', as: 'filtered_workouts', constraints: { year: /\d{4}/, month: /\d{1,2}/ }
+      # year: /\d{4}/: :year パラメータは4桁の数字でなければなりません。
+      # month: /\d{1,2}/: :month パラメータは1桁または2桁の数字でなければなりません。
+    end
   end
+    # get 'workouts/:year/:month', to: 'workouts#index', as: 'filtered_workouts'
+  # 月と年をパラメータとして受け取れるようにします
 end
